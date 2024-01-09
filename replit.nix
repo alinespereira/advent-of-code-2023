e7b2c8938ee339e930994086a1ebf3007c41a1a5
Aline <74569140+alinespereira@users.noreply.github.com>
2024-01-08 21:44:36 -0300

    Rollback  replit.nix

--- replit.nix
+++ replit.nix
@@ -1,59 +1,8 @@
-{ config, lib, pkgs, ... }: {
+{ pkgs }: {
     deps = [
         pkgs.haskellPackages.cabal-install
         pkgs.haskellPackages.hspec-discover
         pkgs.haskellPackages.ghc
         pkgs.haskell-language-server
     ];
-}
-
-with lib;
-
-let
-
-  initScriptBuilder = pkgs.substituteAll {
-    src = ./init-script-builder.sh;
-    isExecutable = true;
-    inherit (pkgs) bash;
-    inherit (config.system.nixos) distroName;
-    path = [pkgs.coreutils pkgs.gnused pkgs.gnugrep];
-  };
-
-in
-
-{
-
-  ###### interface
-
-  options = {
-
-    boot.loader.initScript = {
-
-      enable = mkOption {
-        default = false;
-        type = types.bool;
-        description = lib.mdDoc ''
-          Some systems require a /sbin/init script which is started.
-          Or having it makes starting NixOS easier.
-          This applies to some kind of hosting services and user mode linux.
-
-          Additionally this script will create
-          /boot/init-other-configurations-contents.txt containing
-          contents of remaining configurations. You can copy paste them into
-          /sbin/init manually running a rescue system or such.
-        '';
-      };
-    };
-
-  };
-
-
-  ###### implementation
-
-  config = mkIf config.boot.loader.initScript.enable {
-
-    system.build.installBootLoader = initScriptBuilder;
-
-  };
-
 }
